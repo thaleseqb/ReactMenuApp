bd1cce106460cee4157c28c95b6965c719f09095
thaleseqb <thalesquadros2@gmail.com>
2024-06-30 18:37:38 -0300

    add new pages to the project

---
+++ src/pages/Begin/index.tsx
@@ -0,0 +1,15 @@
+import Menu from "pages/Menu/Itens/itens.json";
+import styles from "./Begin.module.scss"
+
+export default function Begin() {
+    return (
+        <section>
+            <h3>
+                Recomendações da cozinha
+            </h3>
+            <div>
+
+            </div>
+        </section>
+    );
+}
