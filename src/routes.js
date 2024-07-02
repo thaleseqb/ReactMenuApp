00dd3221555f02a10f3b03fc6395ad2bb0df9e9b
thales <thalesquadros2@gmail.com>
2024-07-02 13:39:30 -0300

    create the not found page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import ComponentMenu from "components/ComponentMenu";
 import DefaultPage from "components/DefaultPage";
+import Footer from "components/Footer";
 import About from "pages/About";
 import Begin from "pages/Begin";
 import Menu from "pages/Menu";
+import NotFound from "pages/NotFound";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 export default function AppRouter() {
     return (
-        <main>
+        <main className="container">
             <Router>
                 <ComponentMenu/>
                 <Routes>
@@ -16,7 +18,9 @@ export default function AppRouter() {
                         <Route path="cardapio" element={<Menu/>}/>
                         <Route path="sobre" element={<About/>}/>
                     </Route>
+                    <Route path="*" element={<NotFound/>} />
                 </Routes>
+                <Footer/>
             </Router>
         </main>
     );
